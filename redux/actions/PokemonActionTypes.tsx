10903e9e8fa977ef8c6d87ed4eb90f88abd36d50
Mirkotorrisi <mirko.torrisi92@gmail.com>
2020-10-10 10:45:50 +0200

    bug fixed on CRUD operations

--- redux/actions/PokemonActionTypes.tsx
+++ redux/actions/PokemonActionTypes.tsx
@@ -5,58 +5,56 @@ export const POKEMON_SAVED = "POKEMON_SAVED";
 export const POKEMON_UPDATED = "POKEMON_UPDATED";
 export const POKEMON_DELETED = "POKEMON_DELETED";
 
-export type PokemonType =
-    {
-        forms: PokemonForm[],
-        abilities: PokemonAbility[],
-        sprites: PokemonSprites,
-        stats: PokemonStat[]
-    }
+export type PokemonType = {
+  abilities: PokemonAbility[];
+  sprites: PokemonSprites;
+  stats: PokemonStat[];
+};
 
 export type PokemonAbility = {
-    ability: {
-        name: string,
-        url: string
-    }
-}
-export type PokemonForm = {
-    form: {
-        name: string,
-        url: string
-    }
-}
+  ability: {
+    name: string;
+    url: string;
+  };
+};
+
 export type PokemonSprites = {
-    front_default: string
-}
+  front_default: string;
+};
 
 export type PokemonStat = {
-    base_stat: number,
-    stat: {
-        name: string
-    }
-}
+  base_stat: number;
+  stat: {
+    name: string;
+  };
+};
 export interface PokemonLoading {
-    type: typeof POKEMON_LOADING
+  type: typeof POKEMON_LOADING;
 }
 export interface PokemonFail {
-    type: typeof POKEMON_FAIL
+  type: typeof POKEMON_FAIL;
 }
 export interface PokemonSuccess {
-    type: typeof POKEMON_SUCCESS,
-    payload: PokemonType
+  type: typeof POKEMON_SUCCESS;
+  payload: PokemonType;
 }
 export interface PokemonSaved {
-    type: typeof POKEMON_SAVED,
-    payload: PokemonType[]
+  type: typeof POKEMON_SAVED;
+  payload: PokemonType[];
 }
 export interface PokemonDeleted {
-    type: typeof POKEMON_DELETED,
-    payload: PokemonType[]
+  type: typeof POKEMON_DELETED;
+  payload: PokemonType[];
 }
 export interface PokemonUpdated {
-    type: typeof POKEMON_UPDATED,
-    payload: PokemonType[]
+  type: typeof POKEMON_UPDATED;
+  payload: PokemonType[];
 }
 
-
-export type PokemonDispatchTypes = PokemonLoading | PokemonFail | PokemonSuccess | PokemonSaved | PokemonUpdated | PokemonDeleted 
+export type PokemonDispatchTypes =
+  | PokemonLoading
+  | PokemonFail
+  | PokemonSuccess
+  | PokemonSaved
+  | PokemonUpdated
+  | PokemonDeleted;
--- redux/actions/PokemonActions.tsx
+++ redux/actions/PokemonActions.tsx
@@ -1,52 +1,81 @@
-import { Dispatch } from 'redux'
-import { PokemonDispatchTypes, POKEMON_LOADING, POKEMON_SUCCESS, POKEMON_FAIL, POKEMON_SAVED, POKEMON_DELETED, POKEMON_UPDATED, PokemonType } from './PokemonActionTypes'
-import axios from 'axios'
+import { Dispatch } from "redux";
+import {
+  PokemonDispatchTypes,
+  POKEMON_LOADING,
+  POKEMON_SUCCESS,
+  POKEMON_FAIL,
+  POKEMON_SAVED,
+  POKEMON_DELETED,
+  POKEMON_UPDATED,
+  PokemonType,
+} from "./PokemonActionTypes";
+import axios from "axios";
 
-export const GetPokemon = (pokemon: string) => async (dispatch: Dispatch<PokemonDispatchTypes>) => {
-    pokemon = pokemon.toLowerCase().trim()
+export const GetPokemon = (pokemon: string) => async (
+  dispatch: Dispatch<PokemonDispatchTypes>
+) => {
+  pokemon = pokemon.toLowerCase().trim();
 
-    try {
-        dispatch({
-            type: POKEMON_LOADING
-        })
-        const res = await axios.get(`https://pokeapi.co/api/v2/pokemon/${pokemon}`)
-        dispatch({
-            type: POKEMON_SUCCESS,
-            payload: res.data
-        })
-    }
-    catch (error) {
-        dispatch({
-            type: POKEMON_FAIL
-        })
-    }
-}
-export const BoostPokemon = (pokemon: PokemonType) => (dispatch: Dispatch<PokemonDispatchTypes>) => {
-    for (const iterator of pokemon.stats) {
-        iterator.base_stat = iterator.base_stat + 1
-    }
+  try {
     dispatch({
-        type: POKEMON_SUCCESS,
-        payload: pokemon
-    })
-}
-export const SavePokemon = (newPokemon: PokemonType, pokemonSaved: PokemonType[]) => (dispatch: Dispatch<PokemonDispatchTypes>) => {
-    if ([...pokemonSaved.filter((pokemon) => pokemon.sprites != newPokemon.sprites)].length == 0)
-        dispatch({
-            type: POKEMON_SAVED,
-            payload: [...pokemonSaved, newPokemon]
-        })
-}
-export const DeletePokemon = (pokemonToDel: PokemonType, pokemonSaved: PokemonType[]) => (dispatch: Dispatch<PokemonDispatchTypes>) => {
+      type: POKEMON_LOADING,
+    });
+    const res = await axios.get(`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
     dispatch({
-        type: POKEMON_DELETED,
-        payload: [...pokemonSaved.filter((pokemon) => pokemon.sprites != pokemonToDel.sprites)]
-    })
-}
-export const UpdatePokemon = (pokemonToChange: PokemonType, newPokemon: PokemonType, pokemonSaved: PokemonType[]) => (dispatch: Dispatch<PokemonDispatchTypes>) => {
-    let newArr = [...pokemonSaved.filter((pokemon) => pokemon.sprites != pokemonToChange.sprites)]
+      type: POKEMON_SUCCESS,
+      payload: res.data,
+    });
+  } catch (error) {
     dispatch({
-        type: POKEMON_SAVED,
-        payload: [...newArr, newPokemon]
-    })
-}
+      type: POKEMON_FAIL,
+    });
+  }
+};
+export const BoostPokemon = (pokemon: PokemonType) => (
+  dispatch: Dispatch<PokemonDispatchTypes>
+) => {
+  for (const iterator of pokemon.stats) {
+    iterator.base_stat = iterator.base_stat + 1;
+  }
+  dispatch({
+    type: POKEMON_SUCCESS,
+    payload: pokemon,
+  });
+};
+export const SavePokemon = (
+  newPokemon: PokemonType,
+  pokemonSaved: PokemonType[]
+) => (dispatch: Dispatch<PokemonDispatchTypes>) => {
+  if (
+    pokemonSaved.filter(
+      (pokemon) =>
+        pokemon.sprites.front_default == newPokemon.sprites.front_default
+    ).length == 0
+  )
+    dispatch({
+      type: POKEMON_SAVED,
+      payload: [...pokemonSaved, newPokemon],
+    });
+};
+export const DeletePokemon = (
+  pokemonToDel: PokemonType,
+  pokemonSaved: PokemonType[]
+) => (dispatch: Dispatch<PokemonDispatchTypes>) => {
+  dispatch({
+    type: POKEMON_DELETED,
+    payload: [...pokemonSaved.filter((pokemon) => pokemon != pokemonToDel)],
+  });
+};
+export const UpdatePokemon = (
+  pokemonToChange: PokemonType,
+  newPokemon: PokemonType,
+  pokemonSaved: PokemonType[]
+) => (dispatch: Dispatch<PokemonDispatchTypes>) => {
+  let newArr = [
+    ...pokemonSaved.filter((pokemon) => pokemon != pokemonToChange),
+  ];
+  dispatch({
+    type: POKEMON_UPDATED,
+    payload: [...newArr, newPokemon],
+  });
+};
